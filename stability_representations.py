064c38c58972bc5a0a755cb24e37553978521c31
Xander Dunn <xander@eclode.bio>
2019-05-16 08:33:12 +0000

    Fix bug where the reps could be returned out of order with the ids because of the batching

--- stability_representations.py
+++ stability_representations.py
@@ -16,13 +16,26 @@ np.random.seed(42)
 MODEL_WEIGHT_PATH = "./data/1900_weights"
 
 
-# In[11]:
+# In[39]:
 
 
 import os
 from unirep import mLSTMCell1900, tf_get_shape, aa_seq_to_int
 import pandas as pd
 
+
+def is_valid_seq(seq, max_len=500):
+    """
+    True if seq is valid for the babbler, False otherwise.
+    """
+    l = len(seq)
+    valid_aas = "MRHKDESTNQCUGPAVIFYWLO"
+    if (l < max_len) and set(seq) <= set(valid_aas):
+        return True
+    else:
+        return False
+
+    
 class babbler1900():
 
     def __init__(self, model_path="./data/1900_weights", batch_size=500):
@@ -42,7 +55,6 @@ class babbler1900():
             self._zero_state = sess.run(zero_state)
         
     def get_reps(self, seqs):
-        # Get the input sequences
         seq_ints = [aa_seq_to_int(seq.strip())[:-1] for seq in seqs]
         lengths = [len(x) for x in seq_ints]
         tf_tensor = tf.convert_to_tensor(seq_ints)
@@ -68,32 +80,54 @@ class babbler1900():
             together = np.concatenate((avg_hidden, final_hidden, final_cell), axis=1)
             return together
 
-# Create nets and run inference on a given list of sequences
-def inference_on_seqs(seqs):
-    len_func = lambda x: len(x)
-    len_np = np.vectorize(len_func)
-    seq_lengths = len_np(seqs)
-    unique_lengths = np.unique(seq_lengths)
-    results = None
+# Given a pandas dataframe with a column "sequence", return a list of pandas dataframes grouped by sequence length
+def create_batches(seqs):
+    # Get the unique lengths of all these sequences
+    batches = []
+    lengths = seqs["sequence"].apply(lambda x: len(x))
+    unique_lengths = lengths.unique()
     for unique_length in unique_lengths:
-        boolean_mask = seq_lengths == unique_length
+        boolean_mask = lengths == unique_length
         seqs_of_length = seqs[boolean_mask]
-        print("Getting EclRep representations for {} sequences of length {}...".format(seqs_of_length.shape[0], unique_length))
+        batches += [seqs_of_length]
+    print("There are {} batches".format(len(batches)))
+    return batches
+    
+# Get representations for a numpy array of sequences where all sequences are the same length
+def inference_on_seqs_array(seqs):
         tf.reset_default_graph()
-        model = babbler1900(batch_size=seqs_of_length.shape[0])
-        result = model.get_reps(seqs_of_length)
+        model = babbler1900(batch_size=seqs.shape[0])
+        result = model.get_reps(seqs)
+        return result
+
+# Get representations for a pandas dataframe of sequences in coulmn "sequence"
+def inference_on_seqs(seqs):
+    # Check that all these sequences are valid
+    valid_func = lambda x: is_valid_seq(x)
+    valid_np = np.vectorize(valid_func)
+    valids = valid_np(seqs["sequence"].values)
+    assert False not in valids
+    
+    batches = create_batches(seqs)
+    
+    ids = None
+    reps = pd.DataFrame(columns=list(range(0, 5700)))
+    
+    for batch in batches:
+        print("Getting EclRep representations for {} sequences...".format(batch.shape[0]))
+        reps_new = inference_on_seqs_array(batch["sequence"])
         print("Done")
-        if results is not None:
-            results = np.concatenate((results, result))
+        reps = reps.append(pd.DataFrame(reps_new))
+        if ids is not None:
+            ids = ids.append(batch)
         else:
-            results = result
-    return results
+            ids = batch
+    return ids, reps
     
     
 path = "./data/stability_data"
 df = pd.read_table(os.path.join(path, "ssm2_stability_scores.txt"))
-seqs = df["sequence"].values
-results = inference_on_seqs(seqs)
+ids, results = inference_on_seqs(df)
 print("Got {} results".format(results.shape))
 assert results.shape[0] == seqs.shape[0]
 assert results.shape[1] == 5700
@@ -114,65 +148,15 @@ assert np.array_equal(existing_seqs.index, existing_reps.index)
 # Create reprensetations for some seqs
 print("Checking that these results match the saved truth...")
 for index, row in tqdm(existing_seqs.iterrows(), total=existing_seqs.shape[0]):
-    check_rep = results[index]
+    check_rep = results.iloc[index].values
     true_rep = existing_reps.iloc[index].values
 
-    if not np.allclose(true_rep, check_rep, atol=0.001):
+    if not np.allclose(true_rep, check_rep, atol=0.0001):
         true_check_diff = abs(np.sum(true_rep - check_rep))
         print("{}: {} difference with saved truth".format(index, true_check_diff))
 
 
-# In[20]:
-
-
-# Use tf.data and tf.Variable to feed data into the inference step
-import os
-import tensorflow as tf
-from unirep import aa_seq_to_int, initialize_uninitialized
-import pandas as pd
-
-#using a placeholder
-tf.set_random_seed(42)
-np.random.seed(42)
-
-# data = np.random.sample((100,2))
-# tensor = tf.convert_to_tensor(data)
-# dataset = tf.data.Dataset.from_tensor_slices(tensor)
-# iter = dataset.make_one_shot_iterator()
-# el = iter.get_next()
-# with tf.Session() as sess:
-#     result = sess.run(el) # output [0.37454012 0.95071431]
-#     print(result)
-
-# tf.reset_default_graph() # Reset the graph
-
-def get_reps(model, seqs):
-
-    
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
-#         final_cell, final_hidden, hs = sess.run([final_cell_ts, final_hidden_ts, hs_ts])
-        final_state_, hs = sess.run([model._inference_final_state, model._inference_output],
-                                    feed_dict={
-                                        model._minibatch_x_placeholder: [seq_ints[0]]
-                                    }
-                                   )
-
-        final_cell, final_hidden = final_state_
-        # Drop the batch dimension so it is just seq len by representation size
-        final_cell = final_cell[0]
-        final_hidden = final_hidden[0]
-        hs = hs[0]
-        avg_hidden = np.mean(hs, axis=0)
-        return avg_hidden, final_hidden, final_cell
-    
-path = "./data/stability_data"
-df = pd.read_table(os.path.join(path, "ssm2_stability_scores.txt"))
-seqs = df["sequence"].iloc[:100].values
-get_reps(model, seqs) # TODO: This is only the first 100 rows
-
-
-# In[ ]:
+# In[19]:
 
 
 import os
@@ -181,45 +165,34 @@ import numpy as np
 from tqdm import tqdm_notebook as tqdm
 
 path = "./data/stability_data"
-output_path = os.path.join(path, "stability_with_unirep_fusion.hdf")
+output_path = os.path.join(path, "all_stability_with_eclrep_fusion.hdf")
 
-existing_output = pd.DataFrame(columns=["name", "sequence", "stability"])
-if os.path.isfile(output_path):
-    print("Reading existing output file...")
-    existing_output = pd.read_hdf(output_path, key="ids")
-    print("Got {} existing data points".format(existing_output.shape[0]))
-    duplicates = existing_output.duplicated(subset=["sequence"])
-    assert True not in duplicates.values
+reps_output = pd.DataFrame(columns=list(range(0, 5700)))
 
-new_ids_output = pd.DataFrame(columns=["name", "sequence", "stability"])
-new_reps_output = pd.DataFrame(columns=list(range(0, 5700)))
+# TODO: Assert that there are no duplicate sequences
+# TODO: How do I match the reps with sequences?
 
 for filename in os.listdir(path):
     if filename.endswith(".txt"):
         print("Processing data from {}".format(filename))
         df = pd.read_table(os.path.join(path, filename))
-        model.get_rep(df["sequence"].values)
-        for index, row in tqdm(df.iterrows(), total=df.shape[0]): # TODO: Parallelize this
-            if index != 0 and index % 20 == 0:
-                assert new_ids_output.shape[0] == new_reps_output.shape[0]
-                if new_ids_output.shape[0] > 0:
-                    print("Appending {} points...".format(new_ids_output.shape[0]))
-                    new_ids_output.to_hdf(output_path, index=False, mode="a", key="ids", format="table", append=True)
-                    new_ids_output = pd.DataFrame(columns=["name", "sequence", "stability"])
-                    new_reps_output.to_hdf(output_path, index=False, mode="a", key="reps", format="table", append=True)
-                    new_reps_output = pd.DataFrame(columns=list(range(0, 5700)))
-            # If there is no existing data or the existing data already contains this sequence, ignore it
-            if existing_output.empty or not row["sequence"] in existing_output["sequence"].values:
-                if model.is_valid_seq(row["sequence"], max_len=500):
-                    unirep_fusion = model.get_rep(row["sequence"])
-                    unirep_fusion = np.concatenate((unirep_fusion[0], unirep_fusion[1], unirep_fusion[2]))
-                    print(unirep_fusion.shape)
-                    if "consensus_stability_score" in df.columns:
-                        stability_score = row["consensus_stability_score"]
-                    else:
-                        stability_score = row["stabilityscore"]
-                    new_ids_output.loc[len(new_ids_output)]=[row["name"], row["sequence"], stability_score]
-                    new_reps_output.loc[len(new_reps_output)]=unirep_fusion
+        if "consensus_stability_score" in df.columns:
+            stability_name = "consensus_stability_score"
+        else:
+            stability_name = "stabilityscore"
+        df = df[["name", "sequence", stability_name]]
+        df.rename(columns={'consensus_stability_score': 'stability', 'stabilityscore': 'stability'}, inplace=True)
+        results = inference_on_seqs(df["sequence"].values)
+        print(results.shape)
+        
+#     if model.is_valid_seq(row["sequence"], max_len=500):
+#         unirep_fusion = model.get_rep(row["sequence"])
+#         unirep_fusion = np.concatenate((unirep_fusion[0], unirep_fusion[1], unirep_fusion[2]))
+#         print(unirep_fusion.shape)
+
+
+ids_output.to_hdf(output_path, index=False, mode="a", key="ids", format="fixed")
+reps_output.to_hdf(output_path, index=False, mode="a", key="reps", format="fixed")
 
 
 # In[ ]:
